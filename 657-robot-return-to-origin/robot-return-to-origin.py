2f0d808e88b4f77593a2bc2607f01ec0c6f4f634
Shaik Mahammad Abdul Zaib <shaikzaib49@gmail.com>
2026-07-26 18:30:21 +0530

    Time: 16 ms (35.25%) | Memory: 19.3 MB (53.85%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -0,0 +1,16 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x,y=0,0
+        for move in moves:
+            if move=='R':
+                x+=1
+            elif move=='U':
+                y+=1
+            elif move=="L":
+                x-=1
+            elif move=="D":
+                y-=1
+        if x==0 and y==0:
+            return True
+        else:
+            return False
